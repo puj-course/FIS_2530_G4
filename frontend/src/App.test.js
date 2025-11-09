f5e986280851c1f3aee9ba00a340ddec8f8df1ab
camilaariza <mcamila.ariza25@icloud.com>
2025-11-09 16:25:54 -0500

    test(frontend): agregar pruebas unitarias despliegue

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Frontend funcionando correctamente', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  try {
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  } catch (error) {
+    throw new Error('❌ No se encontró el enlace "learn react" en el DOM');
+  }
 });
+
